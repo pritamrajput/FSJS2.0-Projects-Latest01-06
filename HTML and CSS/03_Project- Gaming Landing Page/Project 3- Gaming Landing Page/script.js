6fca61955ff88c62d89affb03e4162544eb9fc13
Pritam Chauhan <rajputpritam619@gmail.com>
2022-12-24 17:25:50 +0530

    changes made to Project02 and Project03

--- HTML and CSS/03_Project- Gaming Landing Page/Project 3- Gaming Landing Page/script.js
+++ HTML and CSS/03_Project- Gaming Landing Page/Project 3- Gaming Landing Page/script.js
@@ -1,3 +1,8 @@
+
 function onClickMenu(){
-    document.getByElementById("menu").classList.toggle("icon");
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("lists").classList.toggle("wave");
+}
+function onClickBtn(){
+    document.getElementById("btn-1").classList.toggle("click");
 }
